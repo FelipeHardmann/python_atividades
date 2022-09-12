0b73a7dcfd82e4eaa4a6e3c15c2e9ca1888eeb4c
FelipeHardmann <fashardann@gmail.com>
2022-09-12 14:24:25 -0300

    Py

--- ProvasInfinity_Logica1/numeros_primos.py
+++ ProvasInfinity_Logica1/numeros_primos.py
@@ -1,12 +1,14 @@
-number1 = int(input('Digite um número inteiro e positivo: '))
-number2 = int(input('Digite o segundo número inteiro e positivo: '))
-total = 0
+num = input('Número: ')
+cont = 0
 
-for c in range (number1, number2 + 1):
-    if number1 and number2 % c == 0:
-        print('\033[34m')
-        total += 1
-    if total == 0:
-        print('É primo')
-    else:
-        print('Não são primos')
+if num.isdigit():
+    num = int(num)
+
+for i in range(2, num + 1):
+    if num % i == 0:
+        cont += 1
+
+if cont == 1:
+    print('É primo')
+else:
+    print('Não é primo')
--- ProvasInfinity_Logica1/provaIdade.py
+++ ProvasInfinity_Logica1/provaIdade.py
@@ -1,10 +1,10 @@
-'''age = int(input('Digite sua idade: '))
+age = int(input('Digite sua idade: '))
 month = int(input('Digite seu mês de nascimento: '))
 date = int(input('Digite seu dia de nascimento: '))
 
 totalDias = (age * 365) + (month * 30) + date
 
-print(totalDias)'''
+print(totalDias)
 
-for i in range(1,6): 
-    print(f' número: {i}')
+'''for i in range(1,6): 
+    print(f' número: {i}')'''
