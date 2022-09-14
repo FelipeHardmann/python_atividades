d174a6febe7c30f8413bbb4679e11da9c7b391e4
FelipeHardmann <fashardann@gmail.com>
2022-09-14 17:45:39 -0300

    Change roman

--- ProvasInfinity_Logica1/roman_conv2.py
+++ ProvasInfinity_Logica1/roman_conv2.py
@@ -1,7 +1,26 @@
-num_romanos = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 
+'''num_romanos = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 
 'XII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI']
 
 anoNascimento = int(input('Digite o ano que você nasceu: '))
 
 num = num_romanos[anoNascimento // 100]
-print(num)
+print(num)'''
+
+
+
+def converterRoman():
+    num_romanos = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 
+    'XII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI']
+
+    anoAtual = input('Digite o ano: ')
+    if anoAtual.isdigit():
+        anoAtual = int(anoAtual)
+    else:
+        print('Digite um ano')
+        pass
+
+    numero = num_romanos[anoAtual // 100]
+
+    print(numero)
+
+converterRoman()
